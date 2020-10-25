025a424aefcb4f43baf145ca25bcd8b74cda2eaf
Krzysiek <krisxd301@gmail.com>
2020-10-25 13:14:01 +0100

    Update Coronavirus activity. Add app icon

--- app/src/main/java/com/relit/health_care_app/Coronavirus.java
+++ app/src/main/java/com/relit/health_care_app/Coronavirus.java
@@ -1,45 +1,61 @@
 package com.relit.health_care_app;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Button;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.softmoore.android.graphlib.Graph;
+import com.softmoore.android.graphlib.GraphView;
 import com.softmoore.android.graphlib.Point;
 
 import java.util.List;
 import java.util.Map;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceManager;
 
 public class Coronavirus extends AppCompatActivity {
-
     Point[] points;
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_coronavirus);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-        }
-//        Graph graph = new Graph.Builder().build();
-//        GraphView graphView = findViewById(R.id.graph_view);
-//        graphView.setGraph(graph);
+        CreateActivityHelper.setToolbar(this, R.string.covid);
+        findViewById(R.id.button4).setOnClickListener(e -> onBrowser1Click());
+        findViewById(R.id.button5).setOnClickListener(e -> onBrowser2Click());
+        Graph graph = new Graph.Builder().build();
+        GraphView graphView = findViewById(R.id.graph_view);
+        graphView.setGraph(graph);
         SharedPreferences mPrefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         String data = mPrefs.getString("covid-data", null);
         if(data==null)
             getDataFromFireStore();
         else{
-            Log.d("XD", "We have covid data");
+            Log.d("App", "We have covid data");
         }
+    }
+
+    private void onBrowser1Click() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.gov.pl/web/koronawirus"));
+        startActivity(intent);
+    }
+    private void onBrowser2Click() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://news.google.com/covid19/map?hl=pl&mid=%2Fm%2F01bwj7&gl=PL&ceid=PL%3Apl"));
+        startActivity(intent);
     }
 
     private void getDataFromFireStore() {
@@ -56,10 +72,10 @@ public class Coronavirus extends AppCompatActivity {
     }
 
     private void drawGraph() {
-//        Graph graph = new Graph.Builder().setXTicks(new double[]{200, 220, 240, 260, 280, 300}).setYTicks(new double[] {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000}).setWorldCoordinates(-40, 300, -500, 10000)
-//                .setAxes(0, 0).addLineGraph(points, Color.RED).build();
-//        GraphView graphView = findViewById(R.id.graph_view);
-//        graphView.setGraph(graph);
+        Graph graph = new Graph.Builder().setXTicks(new double[]{200, 220, 240, 260, 280, 300}).setYTicks(new double[] {0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000}).setWorldCoordinates(-40, 300, -500, 14000)
+                .setAxes(0, 0).addLineGraph(points, Color.RED).build();
+        GraphView graphView = findViewById(R.id.graph_view);
+        graphView.setGraph(graph);
     }
 
     private void doWorkWithData(Map<String, Object> data) {
@@ -75,7 +91,7 @@ public class Coronavirus extends AppCompatActivity {
             int cases = Integer.parseInt(dataObject.getInfo().split(",")[0]);
             points[i] = new Point(i, cases);
         }
-        //drawGraph();
+        drawGraph();
     }
 
     @Override
